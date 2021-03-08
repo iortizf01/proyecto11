b798d6408a849aaaf4001d396780a7d3750ecbc4
iortiz01 <iortizf01@iesalbarregas.es>
2021-03-08 20:37:05 +0100

    "Validamos el tercer y último cambio"

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo java-git");
 		System.out.println("Primeros cambios");
+		System.out.println("De nuevos cambios");
 	}
 
 }
